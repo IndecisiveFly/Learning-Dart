764d8179150c2d92c86139543d4052b5e8b6053b
Kaitik Kwok <21045837+IndecisiveFly@users.noreply.github.com>
2019-01-16 13:52:46 -0800

    PlayingCards.dart typo fix and added value to card

--- PlayingCards.dart
+++ PlayingCards.dart
@@ -1,11 +1,15 @@
 class card {
   String m_name;
   String m_suit;
+  int m_value;
 
-  card(this.m_name, this.m_suit);
+  card(this.m_name, this.m_suit, this.m_value);
 
   printcard() {
-    print("$m_name of $m_suit" + "s");
+    if (m_name != "Ace")
+      print("$m_name of $m_suit" + "s with a value of $m_value");
+    else
+      print("$m_name of $m_suit" + "s with a value of $m_value or 11");
   }
 }
 
@@ -14,27 +18,35 @@ class deck extends card {
   int position = 0;
 
   //create the deck and shuffle
-  deck() : super('', '') {
+  deck() : super('', '', null) {
     String temp_suit;
     String temp_name;
+    int temp_value;
+
     for (int j = 0; j < 4; j++) {
       if (j == 0) temp_suit = "Spade";
-      if (j == 1) temp_suit = "Queen";
+      if (j == 1) temp_suit = "Heart";
       if (j == 2) temp_suit = "Club";
       if (j == 3) temp_suit = "Diamond";
 
       for (int i = 1; i < 14; i++) {
-        if (i == 1)
+        if (i == 1) {
           temp_name = "Ace";
-        else if (i == 11)
+          temp_value = 1;
+        } else if (i == 11) {
           temp_name = "Jack";
-        else if (i == 12)
+          temp_value = 10;
+        } else if (i == 12) {
           temp_name = "Queen";
-        else if (i == 13)
+          temp_value = 10;
+        } else if (i == 13) {
           temp_name = "King";
-        else
+          temp_value = 10;
+        } else {
           temp_name = i.toString();
-        mydeck.add(card(temp_name, temp_suit));
+          temp_value = i;
+        }
+        mydeck.add(card(temp_name, temp_suit, temp_value));
       }
     }
     assert(mydeck.length == 52);
@@ -54,7 +66,7 @@ class deck extends card {
     position = 0;
     mydeck.shuffle();
   }
-  
+
   //Skip forward one card
   Burn() {
     position++;
